aa127d48c00a7ab5fe0f216dfe5fc88fb72bf373
Hossein Davoodi <H.Davoodi@trenkwalder.com>
2024-02-16 14:52:56 +0300

    Update validation

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { validationSchema } from "./validationSchema";
 
 const FormComponent: React.FC = () => {
@@ -7,16 +8,12 @@ const FormComponent: React.FC = () => {
     register,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm();
+  } = useForm({
+    resolver: zodResolver(validationSchema),
+  });
 
-  const onSubmit = async (data: any) => {
-    try {
-      await validationSchema.parseAsync(data);
-
-      console.log("Form data:", data);
-    } catch (error) {
-      console.error("Validation error:", error);
-    }
+  const onSubmit = (data: any) => {
+    console.log("Form data:", data);
   };
 
   return (
@@ -25,7 +22,7 @@ const FormComponent: React.FC = () => {
       className="flex flex-col gap-8 p-10 lg:w-1/2 md:w-1/2 sm:w-full"
     >
       <div className="flex flex-col gap-2">
-        <label htmlFor="firstName">First Name</label>
+        <label htmlFor="firstName">First Name*</label>
         <input
           type="text"
           {...register("firstName")}
@@ -35,7 +32,7 @@ const FormComponent: React.FC = () => {
       </div>
 
       <div className="flex flex-col gap-2">
-        <label htmlFor="lastName">Last Name</label>
+        <label htmlFor="lastName">Last Name*</label>
         <input
           type="text"
           {...register("lastName")}
@@ -45,7 +42,7 @@ const FormComponent: React.FC = () => {
       </div>
 
       <div className="flex flex-col gap-2">
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email*</label>
         <input
           type="text"
           {...register("email")}
@@ -67,7 +64,7 @@ const FormComponent: React.FC = () => {
       <button
         type="submit"
         disabled={!isValid}
-        className="rounded-full bg-black text-white w-[150px] p-4"
+        className="rounded-full bg-black text-white w-[150px] p-4 disabled:bg-slate-500"
       >
         Submit
       </button>
--- src/components/validationSchema.ts
+++ src/components/validationSchema.ts
@@ -4,5 +4,5 @@ export const validationSchema = z.object({
   firstName: z.string().min(1, "First name is required"),
   lastName: z.string().min(1, "Last name is required"),
   email: z.string().email("Invalid email").min(1, "Email is required"),
-  phone: z.string().min(1, "Phone number is required"),
+  phone: z.string().optional(),
 });
