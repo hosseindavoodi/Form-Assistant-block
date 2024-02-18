c30c5d6be866c1fdaf00a20fbc746e5fabad42e5
Hossein Davoodi <H.Davoodi@trenkwalder.com>
2024-02-18 10:12:24 +0300

    Implement first part of the assistant block logic

--- src/components/FormAssistantBlock.tsx
+++ src/components/FormAssistantBlock.tsx
@@ -1,14 +1,24 @@
-export const FormAssistantBLock = () => {
+import React from "react";
+
+interface FormAssistantBlockProps {
+  setValue: (name: string, value: any) => void;
+}
+
+export const FormAssistantBlock: React.FC<FormAssistantBlockProps> = ({
+  setValue,
+}) => {
   return (
-    <div className=" rounded-lg bg-neutral-300 p-8 ">
+    <div className="rounded-lg bg-neutral-300 p-8">
       <div className="flex flex-col gap-2">
         <strong className="mb-4">
-          Great job! this field is not mandatory but will help if you fill it as
+          Great job! This field is not mandatory but will help if you fill it as
           well
         </strong>
-        <label htmlFor="phone">Phone Number</label>
+        <label htmlFor="phoneAssistant">Phone Number</label>
         <input
           type="text"
+          onChange={(e) => setValue("phone", e.target.value)}
+          name="phone"
           className="border-[#dcdcdc] placeholder:text-[#929292] w-full rounded-lg border bg-white px-4 py-3 !ring-transparent"
         />
       </div>
--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { validationSchema } from "./validationSchema";
-import { FormAssistantBLock } from "./FormAssistantBlock";
+import { FormAssistantBlock } from "./FormAssistantBlock";
 
 const FormComponent: React.FC = () => {
+  const [phoneFieldIsFilled, setPhoneFieldIsFilled] = useState(false);
+  const [isMainForm, setIsMainForm] = useState(true);
   const {
     register,
     handleSubmit,
     reset,
-    formState: { errors, isValid, touchedFields },
+    setValue,
+    watch,
+    formState: { errors, isValid },
   } = useForm({
     resolver: zodResolver(validationSchema),
   });
+
+  const assistantBlockRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleInsideClick = (event: MouseEvent) => {
+      if (
+        assistantBlockRef.current &&
+        assistantBlockRef.current.contains(event.target as Node)
+      ) {
+        setIsMainForm(false);
+      }
+    };
+
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        assistantBlockRef.current &&
+        !assistantBlockRef.current.contains(event.target as Node)
+      ) {
+        setIsMainForm(true);
+      }
+    };
+
+    document.addEventListener("click", handleInsideClick);
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleInsideClick);
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  useEffect(() => {
+    watch((value) => {
+      if (value.phone && isMainForm) {
+        setPhoneFieldIsFilled(true);
+      } else {
+        setPhoneFieldIsFilled(false);
+      }
+    });
+  }, [phoneFieldIsFilled, watch, isMainForm]);
 
   const onSubmit = (data: any) => {
     console.log("Form data:", data);
@@ -31,9 +75,7 @@ const FormComponent: React.FC = () => {
           {...register("firstName")}
           className="border-[#dcdcdc] placeholder:text-[#929292] w-full rounded-lg border bg-white px-4 py-3 !ring-transparent"
         />
-        {touchedFields.firstName && errors.firstName && (
-          <p>{errors.firstName.message?.toString()}</p>
-        )}
+        {errors.firstName && <p>{errors.firstName.message?.toString()}</p>}
       </div>
 
       <div className="flex flex-col gap-2">
@@ -43,9 +85,7 @@ const FormComponent: React.FC = () => {
           {...register("lastName")}
           className="border-[#dcdcdc] placeholder:text-[#929292] w-full rounded-lg border bg-white px-4 py-3 !ring-transparent"
         />
-        {touchedFields.lastName && errors.lastName && (
-          <p>{errors.lastName.message?.toString()}</p>
-        )}
+        {errors.lastName && <p>{errors.lastName.message?.toString()}</p>}
       </div>
 
       <div className="flex flex-col gap-2">
@@ -55,28 +95,31 @@ const FormComponent: React.FC = () => {
           {...register("email")}
           className="border-[#dcdcdc] placeholder:text-[#929292] w-full rounded-lg border bg-white px-4 py-3 !ring-transparent"
         />
-        {touchedFields.email && errors.email && (
-          <p>{errors.email.message?.toString()}</p>
-        )}
+        {errors.email && <p>{errors.email.message?.toString()}</p>}
       </div>
 
       <div className="flex flex-col gap-2">
         <label htmlFor="phone">Phone Number</label>
         <input
           type="text"
-          {...register("phone")}
+          {...register("phone", {})}
           className="border-[#dcdcdc] placeholder:text-[#929292] w-full rounded-lg border bg-white px-4 py-3 !ring-transparent"
         />
       </div>
       <div className="flex flex-col gap-2">
-        <label htmlFor="phone">what is your job?</label>
+        <label htmlFor="job">What is your job?</label>
         <input
           type="text"
           {...register("job")}
           className="border-[#dcdcdc] placeholder:text-[#929292] w-full rounded-lg border bg-white px-4 py-3 !ring-transparent"
         />
       </div>
-      <FormAssistantBLock />
+
+      {!phoneFieldIsFilled && (
+        <div ref={assistantBlockRef}>
+          <FormAssistantBlock setValue={setValue} />
+        </div>
+      )}
 
       <button
         type="submit"
